e2f4c8bb9b6743a0a5c9c78fcc973c14c6b08c00
sjain <jsapan4@gmail.com>
2020-05-24 20:51:54 -0500

    first commit

---
+++ src/test/java/com/jlabs/PatientPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlabs;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PatientPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
